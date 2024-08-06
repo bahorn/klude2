321e308efee130ebeba87c3088c85f1fb908b7a1
B Horn <b@horn.uk>
2024-08-06 03:08:03 +0100

    Setting / Reseting /proc/sys/kernel/printk
    Want to stop our dmesg output from ever showing up on the console.
    
    Signed-off-by: B Horn <b@horn.uk>

--- src/loader-bin/loader.c
+++ src/loader-bin/loader.c
@@ -20,6 +20,18 @@ int delete_module(const char *name, int flags)
     return syscall(SYS_delete_module, name, flags);
 }
 
+void set_printk(char c)
+{
+    int printk_fd = openat(
+        AT_FDCWD, "/proc/sys/kernel/printk", O_WRONLY|O_CREAT|O_TRUNC, 0666
+    );
+    char printk_level[2];
+    printk_level[0] = c;
+    printk_level[1] = '\n';
+    write(printk_fd, printk_level, 2);
+    close(printk_fd);
+}
+
 
 int main()
 {
@@ -34,12 +46,18 @@ int main()
     snprintf(params, 128, "taint_value=%s", taint_buf);
     printf("%s", params);
     /* Read the console printk values */
-    // int printk_fd = open("/proc/sys/kernel/printk", 0);
+    char printk_level = 0x30;
+    int printk_fd = open("/proc/sys/kernel/printk", 0);
+    if (read(printk_fd, &printk_level, 1) < 0) {
+        return -1;
+    }
     /* Lower the console printk values*/
-
+    set_printk('0');
+    
     /* Load + Unload the module */
     init_module(kshelf_loader, kshelf_loader_len, params);
     delete_module(MOD_NAME, 0);
 
     /* Restore the console printk */
+    set_printk(printk_level);
 }
